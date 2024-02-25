de87d7f6993a8d416f6cc61ea655d6bd49bd64bc
russtoo <tokwana@proton.me>
2024-02-25 18:19:09 +0200

    del unused files

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,6 @@ import './assets/css/header.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App loader="coming soon" />
   </React.StrictMode>,
 )
